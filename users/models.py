89ae8abbeb4908a84ca737c01f3848539fa35196
Will Farley <will@django.nu>
2014-04-28 18:01:11 -0600

    adds user profile info

--- users/models.py
+++ users/models.py
@@ -9,7 +9,16 @@ from django.utils.translation import ugettext_lazy as _
 # Subclass AbstractUser
 class UserProfile(models.Model):
 
+	TYPE_CHOICES = (
+		("Voting", "Voting"),
+		("Caucus", "Caucus"),
+		("Association", "Association")
+		)
+
 	user = models.OneToOneField("auth.User")
+	dispensary = models.CharField(max_length=255, blank=True)
+	phone = models.CharField(max_length=20, blank=True)
+	type = models.CharField(max_length=255, choices=TYPE_CHOICES, default="Voting")
 	bio = models.TextField(blank=True)
 
 	def __unicode__(self):
